e8729ffaabb538215266729784f4063706c28598
nxn <6711130023@mut.ac.th>
2026-09-20 23:18:45 +0700

    tttt

--- hello.py
+++ hello.py
@@ -2,8 +2,5 @@ print("helloworld")
 
 print("gggg")
 print("hello world1111")
-<<<<<<< HEAD
 print("hello world2222 ")
-=======
 print("hello world1111")
->>>>>>> d4abdd4ce5edf75bca8d63fe0a1f6ad0ca0091bd
